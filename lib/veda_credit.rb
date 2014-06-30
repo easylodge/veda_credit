a86e00e966df0484a78625b87661ef56f5eedc5a
Andre Mouton <andre@amtek.co.za>
2014-06-30 16:26:34 +0200

    [#517] Remove recursive_open_struct

--- lib/veda_credit.rb
+++ lib/veda_credit.rb
@@ -2,7 +2,6 @@ require "active_record"
 require "veda_credit/version"
 require 'nokogiri'
 require 'httparty'
-require 'recursive-open-struct'
 require 'veda_credit/request'
 require 'veda_credit/response'
 require 'veda_credit/railtie' if defined?(Rails)
