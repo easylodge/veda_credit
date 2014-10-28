5f01caa1696997b74a057002f48779eafcd761c0
Andre Mouton <andre@amtek.co.za>
2014-10-28 23:05:41 +0200

    rename application_id to ref_id

--- lib/generators/templates/migration_rename_table.rb
+++ lib/generators/templates/migration_rename_table.rb
@@ -4,6 +4,7 @@ class RenameTable < ActiveRecord::Migration
     rename_table :veda_credit_responses, :veda_credit_consumer_responses if ((ActiveRecord::Base.connection.table_exists? 'veda_credit_responses') && !(ActiveRecord::Base.connection.table_exists? 'veda_credit_consumer_responses'))
 
     rename_column :veda_credit_consumer_responses, :request_id, :consumer_request_id
+    rename_column :veda_credit_responses, :application_id, :ref_id
     add_column :veda_credit_consumer_responses, :as_hash, :text
   end
   
--- lib/generators/templates/migration_veda_credit_commercial_request.rb
+++ lib/generators/templates/migration_veda_credit_commercial_request.rb
@@ -2,12 +2,11 @@ class CreateVedaCreditCommercialRequest < ActiveRecord::Migration
   def self.up
     unless ActiveRecord::Base.connection.table_exists? 'veda_credit_commercial_requests'
       create_table :veda_credit_commercial_requests do |t|
-        t.integer :application_id
+        t.integer :ref_id
         t.text :xml
         t.text :access
         t.text :service
         t.text :entity
-        t.text :bureau_reference
         t.text :enquiry
         t.timestamps
       end
