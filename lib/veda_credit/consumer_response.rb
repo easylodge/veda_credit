143ba894dec08cf11a91925c4716a6bf2220154a
Heila767 <heila@shuntyard.co.za>
2015-03-15 18:58:54 +0200

    response updates to make it easier to display the results in a view [#1987]

--- lib/veda_credit/consumer_response.rb
+++ lib/veda_credit/consumer_response.rb
@@ -1,16 +1,16 @@
 class VedaCredit::ConsumerResponse < ActiveRecord::Base
   self.table_name = "veda_credit_consumer_responses"
-  
+
   belongs_to :consumer_request, dependent: :destroy
 
   serialize :headers
   serialize :as_hash
-  
+
   validates :consumer_request_id, presence: true
   validates :xml, presence: true
 
   before_save :to_hash
-  
+
   def self.nested_hash_value(obj,key)
     if obj.respond_to?(:key?) && obj.key?(key)
       obj[key]
@@ -28,7 +28,7 @@ class VedaCredit::ConsumerResponse < ActiveRecord::Base
       self.as_hash["BCAmessage"]["BCAservices"]["BCAservice"]["BCAservice_data"]["BCAerror"]["BCAerror_description"]
     elsif product_error
       ("#{product_error["error_type"].humanize} error: #{product_error["input_container"]}, #{product_error["error_description"]}" rescue "There was an Veda product error")
-    else        
+    else
       nil
     end
   end
@@ -38,7 +38,7 @@ class VedaCredit::ConsumerResponse < ActiveRecord::Base
     doc = Nokogiri::XML(self.xml)
     xsd.validate(doc).each do |error|
       error.message
-    end     
+    end
   end
 
   def schema
@@ -55,6 +55,10 @@ class VedaCredit::ConsumerResponse < ActiveRecord::Base
     as_hash["BCAmessage"]["BCAservices"]["BCAservice"]["BCAservice_data"]["response"]["enquiry_report"] rescue {}
   end
 
+  def service_request_id
+    as_hash["BCAmessage"]["service_request_id"] rescue ""
+  end
+
   def primary_match
     enquiry_report["primary_match"]
   end
@@ -68,9 +72,13 @@ class VedaCredit::ConsumerResponse < ActiveRecord::Base
     hsh = {}
     doc.xpath("//summary").each do |el|
       if el.text.present? && (el.text =~ /^\d+$/)
-        hsh[el.xpath("@name").text.underscore] = el.text.to_i
+        val = (el.xpath("@type").text =~ /amount/) ? el.text.to_i : "#{el.text.to_i} #{el.xpath("@type").text}"
+        val = el.text if (el.xpath("@type").text =~ /count/)
+        hsh[el.xpath("@name").text.underscore] = val
       elsif el.text.present?
-        hsh[el.xpath("@name").text.underscore] = el.text
+        val = (el.xpath("@type").text =~ /amount/) ? el.text : "#{el.text} #{el.xpath("@type").text}"
+        val = el.text if (el.xpath("@type").text =~ /count/)
+        hsh[el.xpath("@name").text.underscore] = val
       else
         "nil"
       end
@@ -79,47 +87,52 @@ class VedaCredit::ConsumerResponse < ActiveRecord::Base
   end
 
   def number_of_cross_references
-    primary_match["individual_consumer_credit_file"]["individual_cross_reference"].count rescue 0
+    [primary_match["individual_consumer_credit_file"]["individual_cross_reference"]].flatten.count rescue 0
   end
 
+  #Discharged status: 'not-discharged-not-completed', 'completed', 'discharged'
   def number_of_bankruptcies
-    primary_match["individual_public_data_file"]["bankruptcy"].select{|x| x["discharged_status"].blank? }.count rescue 0
+    [primary_match["individual_public_data_file"]["bankruptcy"].select{|x| x["discharge_status"]["code"] == "not-discharged-not-completed" }].flatten.count rescue 0
+  end
+
+  def discharged_bankruptcies
+    [primary_match["individual_public_data_file"]["bankruptcy"].select{|x| !x["discharge_status"]["code"] == "discharged" }].flatten rescue []
   end
 
   def number_of_discharged_bankruptcies
-    primary_match["individual_public_data_file"]["bankruptcy"].select{|x| !x["discharged_status"].blank? }.count rescue 0
+    self.discharged_bankruptcies.count rescue 0
   end
 
   def number_of_discharged_bankruptcies_last_12_months
-    return 0 unless primary_match["individual_public_data_file"]["bankruptcy"].select{|x| !x["discharged_status"].blank? }.count > 0 rescue false
-    total =  primary_match["individual_public_data_file"]["bankruptcy"].select{|x| !x["discharged_status"].blank? } rescue {}
-    total = total.select{|x| (x["discharged_status"]["date"].to_date >= 12.months.ago rescue false) }.count rescue 0
+    return 0 unless self.discharged_bankruptcies.count > 0 rescue false
+    discharged_bankruptcies.select{|x| (x["discharge_status"]["date"].to_date >= 12.months.ago rescue false) }.count rescue 0
   end
 
   def number_of_part_x_bankruptcies
-    primary_match["individual_public_data_file"]["bankruptcy"].select{|x| x["bankruptcy_type"] == "Personal Insolvency Agreement (Part 10 Deed)" }.count rescue 0
+    return 0 unless self.bankruptcies.count > 0
+    self.bankruptcies.select{|x| x["type"] == "Personal Insolvency Agreement (Part 10 Deed)" }.count rescue 0
   end
 
   def number_of_part_ix_bankruptcies
-    primary_match["individual_public_data_file"]["bankruptcy"].select{|x| x["bankruptcy_type"] == "Debt Agreement (Part 9)" }.count rescue 0
+    return 0 unless self.bankruptcies.count > 0
+    self.bankruptcies.select{|x| x["type"] == "Debt Agreement (Part 9)" }.count rescue 0
   end
 
-
   def number_of_clearout
-    return 0 unless (primary_match["individual_consumer_credit_file"]["default"].count > 0 rescue false)
-    primary_match["individual_consumer_credit_file"]["default"].select{ |key,val| key != "account_details" && val["reason_to_report"] == "Clearout" }.count rescue 0
+    return 0 unless self.defaults.count > 0
+    defaults.select{ |key,val| key != "account_details" && val["reason_to_report"] == "Clearout" }.count rescue 0
   end
 
   def last_36_months_paid_defaults_amount
-    return 0 unless (primary_match["individual_consumer_credit_file"]["default"].count > 0 rescue false)
-    paid_defaults = primary_match["individual_consumer_credit_file"]["default"].select{ |key,val| key != "account_details" && (val["date_recorded"].to_date >= 36.months.ago rescue false ) && val["reason_to_report"] == "Payment Default" }
+    return 0 unless self.defaults.count > 0
+    paid_defaults = defaults.select{ |key,val| key != "account_details" && (val["date_recorded"].to_date >= 36.months.ago rescue false ) && val["reason_to_report"] == "Payment Default" }
     paid_defaults = paid_defaults.collect{|key, val| val["default_amount"]}
     paid_defaults.sum
   end
 
   def last_36_months_unpaid_defaults_amount
-    return 0 unless (primary_match["individual_consumer_credit_file"]["default"].count > 0 rescue false)
-    unpaid_defaults = primary_match["individual_consumer_credit_file"]["default"].select{ |key,val| key != "account_details" && (val["date_recorded"].to_date >= 36.months.ago rescue false ) && val["reason_to_report"] != "Payment Default" }
+    return 0 unless self.defaults.count > 0
+    unpaid_defaults = defaults.select{ |key,val| key != "account_details" && (val["date_recorded"].to_date >= 36.months.ago rescue false ) && val["reason_to_report"] != "Payment Default" }
     unpaid_defaults = unpaid_defaults.collect{|key, val| val["default_amount"]}
     unpaid_defaults.sum
   end
@@ -133,76 +146,125 @@ class VedaCredit::ConsumerResponse < ActiveRecord::Base
   end
 
   def individual
-    return {} unless (primary_match["individual"] rescue false)
+    return [] unless (primary_match["individual"] rescue false)
     hsh = Marshal.load(Marshal.dump(primary_match["individual"]))
-    hsh["first_name"] = hsh["individual_name"]["first_given_name"] rescue nil
-    hsh["surname"] = hsh["individual_name"]["family_name"] rescue nil
+    hsh["full_name"] = [(hsh["individual_name"]["first_given_name"] rescue nil), (hsh["individual_name"]["other_given_name"] rescue nil), (hsh["individual_name"]["family_name"] rescue nil)].join(' ')
     hsh["gender"] = hsh["gender"]["code"] rescue nil
-    if hsh["address"] && hsh["address"].is_a?(Array) && hsh["address"].present?
-      hsh["address"].each do |addr|
-        addr["street_type"] = addr["street_type"]["code"] rescue nil
-        addr["country"] = addr["country"]["country_code"] rescue nil
+
+    if hsh["address"] && hsh["address"].present?
+      addr_hash = []
+      [hsh["address"]].flatten.each do |addr|
+        address_type = addr["type"].gsub('-', ' ').humanize rescue nil
+        address_line_1 = [(addr["street_number"] rescue nil), (addr["street_name"] rescue nil), (addr["street_type"]["code"] rescue nil)].join(' ')
+        address_line_2 = [(addr["suburb"] rescue nil), (addr["state"] rescue nil), (addr["postcode"] rescue nil)].join(' ')
+        complete_address = [address_line_1, address_line_2, (addr["country"]["country_code"] rescue nil)].join(', ')
+        addr_hash << {"address_type" => address_type, "address" => complete_address, "create_date" => (addr["create_date"] rescue nil)}
       end
-    else
-      hsh["address"]["street_type"] = hsh["address"]["street_type"]["code"] rescue nil
-      hsh["address"]["country"] = hsh["address"]["country"]["country_code"] rescue nil
+      hsh["address"] = addr_hash
     end
+    hsh["file_create_date"] = hsh["individual_name"]["create_date"]
+    hsh.delete("employment")
     hsh.delete("individual_name")
-    hsh["address"].delete("create_date")
     hsh
-  end  
+  end
 
   def defaults
-    return {} unless (primary_match["individual_consumer_credit_file"]["default"] rescue false)
+    return [] unless (primary_match["individual_consumer_credit_file"]["default"] rescue false)
     hsh = Marshal.load(Marshal.dump(primary_match["individual_consumer_credit_file"]["default"]))
-    hsh["account_details"]["role"] = hsh["account_details"]["role"]["code"] rescue nil
-    hsh
+    defaults_hash = []
+    [hsh].flatten.each do |default|
+      tmp_hash = {"section" => "Default",
+                  "type" => [(default["account_details"]["account_type"] rescue nil),
+                             (default["account_details"]["default_status"] rescue nil),
+                             (default["original_default"]["reason_to_report"] rescue nil)].reject(&:blank?).join(','),
+                  "date" => (default["original_default"]["date_recorded"] rescue nil),
+                  "creditor" => (default["original_default"]["credit_provider"] rescue nil),
+                  "current_amount" => (default["current_default"]["default_amount"] rescue nil),
+                  "original_amount" => (default["original_default"]["default_amount"] rescue nil),
+                  "role" => (default["account_details"]["role"]["code"] rescue nil),
+                  "reference" => (default["account_details"]["client_reference"] rescue nil)}
+      defaults_hash << tmp_hash
+    end
+    defaults_hash
   end
 
   def credit_enquiries
-    return {} unless (primary_match["individual_consumer_credit_file"]["credit_enquiry"] rescue false)
+    return [] unless (primary_match["individual_consumer_credit_file"]["credit_enquiry"] rescue false)
     hsh = Marshal.load(Marshal.dump(primary_match["individual_consumer_credit_file"]["credit_enquiry"]))
-    if hsh.is_a?(Array) && hsh.present?
-      hsh.each do |cred|
-        cred["role"] = cred["role"]["code"] rescue nil
-      end
-    else
-      hsh["role"] = hsh["role"]["code"] rescue nil
+    [hsh].flatten.each do |cred|
+      cred["role"] = cred["role"]["code"] rescue nil
     end
-    hsh
+    [hsh].flatten
   end
 
   def court_actions
-    return {} unless (primary_match["individual_public_data_file"]["court_action"] rescue false)
+    return [] unless (primary_match["individual_public_data_file"]["court_action"] rescue false)
     hsh = Marshal.load(Marshal.dump(primary_match["individual_public_data_file"]["court_action"]))
-    if hsh.is_a?(Array) && hsh.present?
-      hsh.each do |el|
-        el["role"] = el["role"]["code"] rescue nil
-      end
-    else
-      hsh["role"] = hsh["role"]["code"] rescue nil
+    court_action_hash = []
+    [hsh].flatten.each do |ca|
+      tmp_hash = {"section" => "Court Action",
+                  "type" => [(ca["type"].gsub('-', ' ').humanize rescue nil),
+                             (ca["court_type"] rescue nil)].reject(&:blank?).join(','),
+                  "date" => (ca["action_date"] rescue nil),
+                  "creditor" => (ca["creditor"] rescue nil),
+                  "court_action_amount" => (ca["court_action_amount"].to_i rescue nil),
+                  "role" => (ca["role"]["code"] rescue nil),
+                  "reference" => (ca["plaint_number"] rescue nil)}
+      court_action_hash << tmp_hash
     end
-    hsh 
+    court_action_hash
   end
 
-  def directorship
-    return {} unless (primary_match["individual_public_data_file"]["directorship"] rescue false)
+  def directorships
+    return [] unless (primary_match["individual_public_data_file"]["directorship"] rescue false)
     hsh = Marshal.load(Marshal.dump(primary_match["individual_public_data_file"]["directorship"]))
-    hsh["organisation"]["organisation_type"] = hsh["organisation"]["organisation_type"]["code"]
-    hsh
+    [hsh].flatten.each do |el|
+      el["organisation_bureau_reference"] = el["organisation"]["bureau_reference"] rescue nil
+      el["organisation_name"] = "#{el["organisation"]["organisation_name"]} #{el["organisation"]["organisation_type"]["code"]}" rescue nil
+      el.delete("organisation")
+    end
+    [hsh].flatten
   end
 
-  def bankruptcy
-    return {} unless (primary_match["individual_public_data_file"]["directorship"] rescue false)
+  def bankruptcies
+    return [] unless (primary_match["individual_public_data_file"]["bankruptcy"] rescue false)
     hsh = Marshal.load(Marshal.dump(primary_match["individual_public_data_file"]["bankruptcy"]))
-    hsh["role"] = hsh["role"]["code"] rescue nil
-    hsh
+    bankrupt_hash = []
+    [hsh].flatten.each do |bnkr|
+      tmp_hash = {"section" => "Bankruptcy",
+                  "type" => [(bnkr["bankruptcy_type"] rescue nil),
+                             (bnkr["proceedings"]["proceedings_status"]["code"] rescue nil)].reject(&:blank?).join(','),
+                  "date" => (bnkr["date_declared"] rescue nil),
+                  "role" => (bnkr["role"]["code"] rescue nil),
+                  "discharge_status" => (bnkr["discharge_status"]["code"] rescue nil)}
+      bankrupt_hash << tmp_hash
+    end
+    bankrupt_hash
   end
 
-  private  
+  def cross_references
+    return [] unless (primary_match["individual_consumer_credit_file"]["individual_cross_reference"] rescue false)
+    hsh = Marshal.load(Marshal.dump(primary_match["individual_consumer_credit_file"]["individual_cross_reference"]))
+    [hsh].flatten.each do |el|
+      el["individual"] = [(el["individual_name"]["first_given_name"] rescue nil), (el["individual_name"]["other_given_name"] rescue nil), (el["individual_name"]["family_name"] rescue nil)].join(' ')
+      el.delete("individual_name")
+    end
+    [hsh].flatten
+  end
+
+  def employment_histories
+    return [] unless (primary_match["individual"]["employment"] rescue false)
+    hsh = Marshal.load(Marshal.dump(primary_match["individual"]["employment"]))
+    [hsh].flatten
+  end
+
+  private
   def to_hash
     return nil unless self.xml.present?
-    new_xml = self.xml.gsub('gender type','gender code').gsub('role type','role code')
+    new_xml = self.xml.gsub('gender type','gender code')\
+                      .gsub('role type','role code')\
+                      .gsub('proceedings-status type','proceedings-status code')\
+                      .gsub('discharge-status type','discharge-status code')
     self.as_hash = Hash.from_xml(new_xml)
   end
 
