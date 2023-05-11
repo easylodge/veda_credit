63e6192aacde8312b85e4aab68d5b738019b3834
monikavermaa <mv@easylodge.com.au>
2023-05-11 11:49:02 +0530

    6857 update schema..

--- spec/schema.rb
+++ spec/schema.rb
@@ -42,4 +42,23 @@ ActiveRecord::Schema.define do
     t.integer :consumer_request_id
     t.timestamps
   end
+
+  create_table :equifax_idmatrix_requests do |t|
+    t.integer :ref_id
+    t.text :xml
+    t.text :soap
+    t.text :access
+    t.text :entity
+    t.text :enquiry
+    t.timestamps
+  end
+
+  create_table :equifax_idmatrix_responses do |t|
+    t.text :headers
+    t.integer :code
+    t.text :xml
+    t.boolean :success
+    t.integer :request_id
+    t.timestamps
+  end
 end
